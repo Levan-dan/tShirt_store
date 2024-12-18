153bc39922fc736e2518886011f2bcc96dbc168b
vandan <levandanhh2@gmail.com>
2024-12-18 10:37:08 +0700

    hienThiSanPhamRaManHinhKhachHang

--- src/main/java/com/example/tshirt_store/service/StoreServiceInterface.java
+++ src/main/java/com/example/tshirt_store/service/StoreServiceInterface.java
@@ -1,9 +1,14 @@
 package com.example.tshirt_store.service;
 
+import com.example.tshirt_store.modle.Product;
 import com.example.tshirt_store.modle.User;
+
+import java.util.List;
 
 public interface StoreServiceInterface {
     void actionSignUp(User user);
     User actionLogin(String name, String pass);
+
+    List<Product> showProduct();
 
 }
